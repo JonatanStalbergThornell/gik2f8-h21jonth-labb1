28cc314d7f67cbc49e4341ca8bbff5c70e7d8ca2
h21jonth <jonatanthornell@gmail.com>
2022-12-07 15:09:26 +0100

    Skapat mouseover-effekt

---
+++ gik2f8-ht22-01-searchlist-p03/Components/BookInfo.js
@@ -0,0 +1,11 @@
+
+const BookInfo = (book) => {
+  let html = `
+  <div id="bookDetail" class="book__details absolute right-12 rounded p-2 w-1/6 top-10       
+  flex flex-col text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 border-2	border-gray-900	">
+      <img src="${book.coverImage}" alt="Bokomslag saknas"> 
+  </div>
+  `;
+
+  return html;
+}
--- gik2f8-ht22-01-searchlist-p03/script.js
+++ gik2f8-ht22-01-searchlist-p03/script.js
@@ -18,6 +18,8 @@ searchField.addEventListener('keyup', (e) =>
   )
 );
 
+
+
 function renderBookList(bookList) {
   const existingElement = document.querySelector('.book-list');
 
@@ -25,4 +27,65 @@ function renderBookList(bookList) {
 
   existingElement && root.removeChild(existingElement);
   bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
+};
+
+
+function renderBookList(bookList) {
+  const existingElement = document.querySelector('.book-list');
+  const root = document.getElementById('root');
+
+  existingElement && root.removeChild(existingElement);
+  bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
+
+
+  const elements = document.querySelectorAll(".book-list__item"); 
+
+  for(let i = 0; i < elements.length; i++){ 
+
+    elements[i].addEventListener("mousemove", (e) => {
+      let book = getBookDetails(e.target.id)
+      book.then(function (result){
+        renderBookItem(result);
+
+          let div = document.getElementById('bookDetail');
+          let left = e.pageX + 30;
+          let top = e.pageY + -50;
+          div.style.left = left + 'px';
+          div.style.top = top + 'px';
+        
+      })
+    });
+
+
+   
+
+    elements[i].addEventListener("mouseleave", () => {
+      const existElement = document.getElementById("bookDetail");
+      existElement && existElement.remove();
+    });
+
+
+  
+  
+  }
+}
+
+
+    
+
+
+
+
+function renderBookItem(book){
+
+  const existElement = document.getElementById("bookDetail");
+  const root = document.getElementById("root");
+
+  existElement && existElement.remove();
+
+  let html = BookInfo(book);
+  
+  root.insertAdjacentHTML("afterend", html);
+  
+
 }
